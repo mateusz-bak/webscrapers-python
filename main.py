060d845d6969e60525b5ab4dbb22908981a65110
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2021-12-14 12:19:01 +0000

    Corrected ads.json file read error. Added error notification.

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ import datetime
 import time
 
 
-def send_notification(ad):
+def send_notification_ad(ad):
     msg = ad["ad"] + "\n"
     msg += ad["contact"] + "\n\n"
     msg += datetime.datetime.now().strftime('%d-%m-%Y %H:%M') + "\n\n" + "____________"
+    execute_docker(msg)
 
+
+def send_notification_error(e):
+    msg = type(e) + "\n"
+    msg += e.args + "\n"
+    msg += e + "\n"
+    msg += datetime.datetime.now().strftime('%d-%m-%Y %H:%M') + "\n" + "____________"
     execute_docker(msg)
 
 
@@ -40,19 +47,24 @@ with open('webpage.html', 'r') as html_file:
         ad_dict = {"ad": ad_parts[0].text, "contact": ad_parts[1].text}
         new_ads.append(ad_dict)
 
+    new_ads.reverse()
+
     try:
         with open('ads.json', 'r') as fin:
             old_ads = json.load(fin)
-    except IOError:
-        old_ads = []
 
-    new_ads.reverse()
-    with open('ads.json', 'w') as fout:
-        json.dump(new_ads, fout)
+            for new_ad in new_ads:
+                if new_ad not in old_ads:
+                    send_notification_ad(new_ad)
+                    old_ads.append(new_ad)
+                    time.sleep(10)
 
-    for new_ad in new_ads:
-        if new_ad not in old_ads:
-            send_notification(new_ad)
-            time.sleep(10)
+            with open('ads.json', 'w') as fout:
+                json.dump(old_ads, fout)
+    except IOError as e:
+        send_notification_error(e)
+
+
+
 
 
